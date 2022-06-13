1945a738fc8d0328d75c265dec82434e2a3dd1fc
makeevmr <makeevmr@m2.ru>
2022-06-13 17:38:51 +0300

    добавил 2 теста, убрал лишний ассерт

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,23 +1,23 @@
 import chai from 'chai';
-import {run, stop} from "../lib/browser.js";
+import {run, stop, login} from "../lib/browser.js";
 
 const assert = chai.assert;
 
-describe ('Главная страничка', () => {
+describe ('Testing Vikunja', () => {
     let page;
-    const profileNameField = '.navbar-end > .user > .dropdown > .base-button > .username';
-    const passwordField = '#password';
-    const usernameField = '#username';
-    const loginBtn = '.is-primary';
-
     const createAccountBtn = 'div > div > #loginform > .mt-2 > a';
-    const createAccountText = '.no-auth-wrapper > .noauth-container > .content > div > .title';
-
+    const mainPageLoginText = '.no-auth-wrapper > .noauth-container > .content > div > .title';
     const changeVikunjaInstallationBtn = '.content > div > .api-config > .api-url-info > a';
     const changeVikunjaInstallation = '.api-config > div > .field > .control > .base-button';
     const changeVikunjaInstallationSuccessText = '.vue-notification-group > span > .vue-notification-wrapper > .vue-notification-template > .notification-content';
     const forgotYourPasswordBtn = 'div > #loginform > .field > .label-with-link > .reset-password-link';
     const sendMeAPasswordBtn = 'div > form > .field > .control > .base-button--type-button';
+    const profileDropdownMenu = '.menu-active > .navbar-end > .user > .dropdown > .base-button';
+    const profileDropdownMenuLogout = '.user > .dropdown > .dropdown-menu > .dropdown-content > .base-button:nth-child(4)';
+    const profileDropdownMenuAbout = '.user > .dropdown > .dropdown-menu > .dropdown-content > .base-button:nth-child(3)';
+    const profileDropdownAboutModalCloseBtn = '.card > .card-content > .content > .modal-card-foot > .base-button';
+    const profileDropdownAboutModalXsign = '.card > .card-header > .card-header-icon > .icon > .svg-inline--fa';
+
 
     beforeEach(async () => {
         page = await run('https://try.vikunja.io/');
@@ -26,22 +26,11 @@ describe ('Главная страничка', () => {
         await stop();
     });
 
-    it('Авторизировать демо пользователя', async () => {
-        await page.click(usernameField);
-        await page.fill(usernameField, 'demo');
-        await page.click(passwordField);
-        await page.fill(passwordField, 'demo');
-        await page.click(loginBtn);
-        await page.waitForSelector(profileNameField);
-        const profileNameText = await page.textContent(profileNameField);
-        assert.strictEqual(profileNameText, 'demo', 'Имя пользователя не равно demo');
-    });
-
     it('Переход на страничку регистрации через "Create account"', async () => {
         await page.waitForSelector(createAccountBtn);
         await page.click(createAccountBtn);
-        const сreateAccountTextField = await page.textContent(createAccountText);
-        assert.strictEqual(сreateAccountTextField, 'Create account', 'Текст Create Account не найден');
+        const createAccountTextField = await page.textContent(mainPageLoginText);
+        assert.strictEqual(createAccountTextField, 'Create account', 'Текст Create Account не найден');
     });
 
     it('Изменение Vikunja URL', async () => {
@@ -51,7 +40,7 @@ describe ('Главная страничка', () => {
         await page.click(changeVikunjaInstallation);
         await page.waitForSelector(changeVikunjaInstallationSuccessText);
         const InstallationSuccessText =  await page.textContent(changeVikunjaInstallationSuccessText);
-        assert.strictEqual(InstallationSuccessText, 'Using Vikunja installation at "try.vikunja.io".', 'Текст "Using Vikunja installation at "try.vikunja.io". не найден');
+        assert.strictEqual(InstallationSuccessText, 'Используется Vikunja на "try.vikunja.io".', 'Текст "Используется Vikunja на "try.vikunja.io"." не найден');
     });
 
     it('Переход на страницу восстановления пароля через кнопку Forgot Your Password', async () => {
@@ -59,6 +48,26 @@ describe ('Главная страничка', () => {
         await page.click(forgotYourPasswordBtn);
         await page.waitForSelector(sendMeAPasswordBtn);
         const sendMeAPasswordBtnText = await page.textContent(sendMeAPasswordBtn);
-        assert.strictEqual(sendMeAPasswordBtnText, 'Send me a password reset link', 'Текст "Send me a password reset link" не найден');
+        assert.strictEqual(sendMeAPasswordBtnText, 'Отправить ссылку на сброс пароля', 'Текст "Отправить ссылку на сброс пароля" не найден');
+    });
+
+    it('Logout через дропдаун-меню в профиле', async () => {
+        await login();
+        await page.waitForSelector(profileDropdownMenu);
+        await page.click(profileDropdownMenu);
+        await page.waitForSelector(profileDropdownMenuLogout);
+        await page.click(profileDropdownMenuLogout);
+        const createAccountTextField = await page.textContent(mainPageLoginText);
+        assert.strictEqual(createAccountTextField, 'Войти', 'Текст Войти не найден');
+    });
+
+    it('Открытие About через дропдаун-меню в профиле', async () => {
+        await login();
+        await page.waitForSelector(profileDropdownMenu);
+        await page.click(profileDropdownMenu);
+        await page.waitForSelector(profileDropdownMenuAbout);
+        await page.click(profileDropdownMenuAbout);
+        await page.waitForSelector(profileDropdownAboutModalCloseBtn);
+        await page.waitForSelector(profileDropdownAboutModalXsign);
     });
 });
